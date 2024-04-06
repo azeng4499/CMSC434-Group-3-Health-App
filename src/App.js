c0123e1d57d4cc6f50eed2a50869ed4418b9541e
Trajan Mulinazzi <tmulinaz@umd.edu>
2024-04-06 11:59:47 -0400

    04/06

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
 import Timer from "./components/timer/timer";
+
+
 function App() {
   return (
-    <div style={{ width: "100vw", height: "100vh" }}>
-      <Timer />
-    </div>
+    <div style={{ width: "100vw", height: "100vh" }}>{/* <Timer /> */}</div>
   );
 }
 
