35711c11a6e7d8e99c58b3c4f480dc575de66944
Aaron Zeng <aaronz2003@gmail.com>
2024-04-06 11:50:25 -0400

    tabs

---
+++ src/components/home/home.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Nav from "../shared/nav/nav";
+
+const Home = ({ setPage, showHamburgerMenu, setShowHamburgerMenu }) => {
+  return (
+    <div className="w-screen h-screen">
+      <Nav
+        setPage={setPage}
+        showHamburgerMenu={showHamburgerMenu}
+        setShowHamburgerMenu={setShowHamburgerMenu}
+      />
+      <div
+        className="w-full overflow-hidden"
+        style={{ height: "calc(100vh - 82.8px)" }}
+      >
+        {/* Home code */}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
